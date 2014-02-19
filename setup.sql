1736470c82516c1f3a9428257d6e180dbe3ddfd9
Keith <kiwimanman@gmail.com>
2014-02-18 23:35:25 -0800

    Updated setup.sql to add something into each table and placed a bunch of sane defaults to hopefully make this project easier down the line.

--- setup.sql
+++ setup.sql
@@ -1,33 +1,39 @@
 CREATE TABLE RentalPlan (
-    id              INTEGER PRIMARY KEY,
+    id              INTEGER IDENTITY(1,1) PRIMARY KEY,
     name            VARCHAR(MAX) NOT NULL,
     maximum_rentals INTEGER NOT NULL,
     monthly_fee     MONEY NOT NULL
 );
 
 CREATE TABLE Customer (
-    id         INTEGER PRIMARY KEY,
-    login      VARCHAR(MAX) NOT NULL,
-    password   VARCHAR(MAX) NOT NULL,
-    first_name VARCHAR(MAX) NOT NULL,
-    last_lname VARCHAR(MAX) NOT NULL,
-    plan_id    INTEGER REFERENCES RentalPlan NOT NULL
-);
+      id         INTEGER IDENTITY(1,1) PRIMARY KEY,
+      login      VARCHAR(MAX) NOT NULL,
+      password   VARCHAR(MAX) NOT NULL,
+      fname VARCHAR(MAX) NOT NULL,
+      lname VARCHAR(MAX) NOT NULL,
+      plan_id    INTEGER REFERENCES RentalPlan NOT NULL
+  );
 
-CREATE TABLE Rental (
-    customer_id   INTEGER NOT NULL REFERENCES Customer,
-    movie_id      INTEGER NOT NULL,
-    status        BIT NOT NULL,
-    checkout_date DATE NOT NULL,
-    checkout_time TIME NOT NULL
-);
+  CREATE TABLE Rental (
+      customer_id   INTEGER NOT NULL REFERENCES Customer,
+      movie_id      INTEGER NOT NULL,
+      status        BIT NOT NULL DEFAULT(1),
+      checkout      DATETIME NOT NULL DEFAULT GETDATE()
+  );
 
-CREATE CLUSTERED INDEX Customer_Index       ON Customer(id);
-CREATE CLUSTERED INDEX RentalPlan_Index     ON RentalPlan(id);
 CREATE CLUSTERED INDEX Rental_Index         ON Rental(customer_id);
 
-INSERT INTO RentalPlan (id, plan_name, maximum_rentals, monthly_fee)
-VALUES (1, 'Basic Plan',                    1, 9.99);
-       (2, 'Might As Well Plan',            2, 12.99),
-       (3, 'Big ol Plan',                   5, 20.00),
-       (4, 'We will not be open long plan', 100, 99.99);
+INSERT INTO RentalPlan (name, maximum_rentals, monthly_fee)
+VALUES ('Basic Plan',                    1, 9.99),
+       ('Might As Well Plan',            2, 12.99),
+       ('Big ol Plan',                   5, 20.00),
+       ('We will not be open long plan', 100, 99.99);
+
+INSERT INTO Customer (login, password, fname, lname, plan_id)
+VALUES ('username',   'password', 'User',     'Name',  1),
+       ('ruby',       'password', 'Yukihiro', 'Matz',  2),
+       ('kiwimanman', 'password', 'Keith',    'Stone', 3),
+       ('suki',       'password', 'My',       'Cat',   4);
+
+INSERT INTO Rental (customer_id, movie_id)
+VALUES (3, 255074);
